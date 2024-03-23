3267b837eb4bfccf318c8086269550c8b184727b
Zaid khan <160598429+Zaidkhan009@users.noreply.github.com>
2024-03-23 08:02:24 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+var users = ['usama', 'azeem', 'bisma', 'siddique', 'admin zaid'];
+if (users.length === 0) {
+    console.log("We need to find some user! ");
+}
+else {
+    users = [];
+    console.log("All user have been removed " + users.length);
+}
---
+++ app.ts
@@ -0,0 +1,10 @@
+// let users : string [] = ['usama','azeem','bisma','siddique','admin zaid'] 
+
+// if (users.length === 0) {
+//     console.log ("We need to find some user! ")
+// }
+
+// else{
+//     users = [];
+//     console.log("All user have been removed" + users.length)
+// }
